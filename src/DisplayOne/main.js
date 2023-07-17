f7c46cee3175b868e8806800999f1399b6ed2c59
thanhdc57 <115173618+thanhdc57@users.noreply.github.com>
2023-07-17 09:24:07 +0700

    Update main.js

--- src/DisplayOne/main.js
+++ src/DisplayOne/main.js
@@ -1,24 +1,16 @@
 document.addEventListener("click", function (e) {
     let clickEd = e.target;
-    let eduHeader = document.querySelector(".edu-header");
-    let expHeader = document.querySelector(".exp-header");
     let expTable = document.querySelector(".exp-table");
     let eduTable = document.querySelector(".edu-table");
 
     if (clickEd.classList.contains("exp-header")) {
-        expTable.style.display = "table";
-        if (clickEd.classList.contains("change-background")) {
-            clickEd.classList.remove("change-background");
-            eduHeader.classList.add("change-background");
-            eduTable.style.display = "none";
-        }
+        console.log("exp");
+        expTable.classList.remove("d-none");
+        eduTable.classList.add("d-none");
     }
     if (clickEd.classList.contains("edu-header")) {
-        eduTable.style.display = "table";
-        if (clickEd.classList.contains("change-background")) {
-            clickEd.classList.remove("change-background");
-            expHeader.classList.add("change-background");
-            expTable.style.display = "none";
-        }
+        console.log("edu");
+        eduTable.classList.remove("d-none");
+        expTable.classList.add("d-none");
     }
 })
