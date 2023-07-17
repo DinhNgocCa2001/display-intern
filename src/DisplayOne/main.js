2d0cf33c8e9e10057b1ab8d9e2bc58b233cacadc
Cadozvex <tnga6642@gmail.com>
2023-07-17 15:32:48 +0700

    Add rename file css

--- src/DisplayOne/main.js
+++ src/DisplayOne/main.js
@@ -1,24 +1,24 @@
 document.addEventListener("click", function (e) {
-    let clickEd = e.target;
-    let eduHeader = document.querySelector(".edu-header");
-    let expHeader = document.querySelector(".exp-header");
-    let expTable = document.querySelector(".exp-table");
-    let eduTable = document.querySelector(".edu-table");
+  let clickEd = e.target;
+  let eduHeader = document.querySelector(".edu-header");
+  let expHeader = document.querySelector(".exp-header");
+  let expTable = document.querySelector(".exp-table");
+  let eduTable = document.querySelector(".edu-table");
 
-    if (clickEd.classList.contains("exp-header")) {
-        expTable.style.display = "table";
-        if (clickEd.classList.contains("change-background")) {
-            clickEd.classList.remove("change-background");
-            eduHeader.classList.add("change-background");
-            eduTable.style.display = "none";
-        }
+  if (clickEd.classList.contains("exp-header")) {
+    expTable.style.display = "table";
+    if (clickEd.classList.contains("change-background")) {
+      clickEd.classList.remove("change-background");
+      eduHeader.classList.add("change-background");
+      eduTable.style.display = "none";
     }
-    if (clickEd.classList.contains("edu-header")) {
-        eduTable.style.display = "table";
-        if (clickEd.classList.contains("change-background")) {
-            clickEd.classList.remove("change-background");
-            expHeader.classList.add("change-background");
-            expTable.style.display = "none";
-        }
+  }
+  if (clickEd.classList.contains("edu-header")) {
+    eduTable.style.display = "table";
+    if (clickEd.classList.contains("change-background")) {
+      clickEd.classList.remove("change-background");
+      expHeader.classList.add("change-background");
+      expTable.style.display = "none";
     }
-})
+  }
+});
